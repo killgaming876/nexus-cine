cfa5838923874c1dbc385e4e93f6f6320ea0183d
BloxFruitsScripters <131740181+killgaming876@users.noreply.github.com>
2026-08-14 11:40:38 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-
-  basePath: "/nexus-cine",
-
-  assetPrefix: "/nexus-cine/",
-
+  output: 'export',
+  basePath: '/nexus-cine',
+  assetPrefix: '/nexus-cine/',
   images: {
     unoptimized: true,
   },
-
-  trailingSlash: true,
 };
 
 module.exports = nextConfig;
